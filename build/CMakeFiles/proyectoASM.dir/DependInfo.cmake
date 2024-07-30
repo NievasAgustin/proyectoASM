a32087a87b136382a7a4bfeb01e2e64f4f8ef3a4
NievasAgustin <37403818+NievasAgustin@users.noreply.github.com>
2024-07-29 22:55:08 -0300

    adding build Folder

---
+++ build/CMakeFiles/proyectoASM.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/ucc4/media/disk/_cygdrive_D_NIEVAS_Documents_2020_Ucc_Org_y_Arq_Raspberry_proyectoASM/arbol.s" "/home/ucc4/media/disk/_cygdrive_D_NIEVAS_Documents_2020_Ucc_Org_y_Arq_Raspberry_proyectoASM/build/CMakeFiles/proyectoASM.dir/arbol.s.o"
+  "/home/ucc4/media/disk/_cygdrive_D_NIEVAS_Documents_2020_Ucc_Org_y_Arq_Raspberry_proyectoASM/proyectoASM.s" "/home/ucc4/media/disk/_cygdrive_D_NIEVAS_Documents_2020_Ucc_Org_y_Arq_Raspberry_proyectoASM/build/CMakeFiles/proyectoASM.dir/proyectoASM.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../proyectoASM"
+  "/usr/include/SDL2"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ucc4/media/disk/_cygdrive_D_NIEVAS_Documents_2020_Ucc_Org_y_Arq_Raspberry_proyectoASM/main.cpp" "/home/ucc4/media/disk/_cygdrive_D_NIEVAS_Documents_2020_Ucc_Org_y_Arq_Raspberry_proyectoASM/build/CMakeFiles/proyectoASM.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../proyectoASM"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/proyectoASM.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyectoASM.dir/arbol.s.o"
+  "CMakeFiles/proyectoASM.dir/main.cpp.o"
+  "CMakeFiles/proyectoASM.dir/proyectoASM.s.o"
+  "proyectoASM"
+  "proyectoASM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM CXX)
+  include(CMakeFiles/proyectoASM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
